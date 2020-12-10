e6605aead52b494f7927a3cd2d93d65ad9f4f677
AggieWB <usmaggie23@gmail.com>
2020-12-10 11:09:35 -0800

    Import Input component, React, and useState
    Add change() and submit() functions
    Initialize text, setText, list, and setList with useState() hooks function
    Add Input functional component to return div

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import React, { useState } from 'react';
+import Input from './components/Input.js';
+
 //React hooks reference: https://reactjs.org/docs/hooks-overview.html
 
 function App() {
+  const [text, setText] = useState('');
+  const [list, setList] = useState([]);
+  const change = (event) => {
+      const text = event.target.value;
+      setText(text);
+  }
+
+  const submit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    setList(list.concat(text));
+    setText('');
+  }
+
   return <div className="App">
-    </div>
+      <Input change={change} submit={submit} text={text}/>
+    </div>;
 }
 
 export default App;
