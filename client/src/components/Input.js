092e8e12959317b48121a3b52b1b3d595ef120c6
AggieWB <usmaggie23@gmail.com>
2020-12-10 12:37:44 -0800

    Add required keyword to text input

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -4,7 +4,7 @@ const Input = (props) => {
     return <form onSubmit={props.submit}>
         <label>
             Enter an item or items separated by commas:
-            <input type="text" value={props.text} onChange={props.change}></input>
+            <input type="text" value={props.text} onChange={props.change} required></input>
         </label>
         <input type="submit" value="Add item(s)"></input>
     </form>;
