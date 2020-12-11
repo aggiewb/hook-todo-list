33e263d0e3ff2f564babf449748799ac20d0a261
AggieWB <usmaggie23@gmail.com>
2020-12-11 09:33:16 -0800

    Implement removeItem() to remove a single item from the list

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Display from './components/Display.js';
 function App() {
   const [text, setText] = useState('');
   const [list, setList] = useState([]);
+  
   const change = (event) => {
       const text = event.target.value;
       setText(text);
@@ -20,10 +21,17 @@ function App() {
     setText('');
   }
 
+  const removeItem = (event) => {
+    const liElement = event.target.parentNode;
+    const liElementId = liElement.id;
+    const newList = list.filter(item => item !== liElementId);
+    setList(newList);
+  }
+
   return <div className="App">
       <h1>React Hook TODO List</h1>
       <Input change={change} submit={submit} text={text}/>
-      <Display list={list}/>
+      <Display list={list} removeItem={removeItem}/>
     </div>;
 }
 
--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import xIcon from '../media/x-icon.png';
 
-//Add ability to remove single items and clear list
+//Add ability to clear list
 const Display = (props) => {
     return <ul>
-        {props.list.map(item => <li key={item}>{item}<img src={xIcon} alt={"A red X that can be clicked to remove this item from the list"}></img></li>)}
+        {props.list.map(item => {
+            return <li key={item} id={item}>{item}
+                <img onClick={props.removeItem} src={xIcon} alt={"A red X that can be clicked to remove this item from the list"}></img>
+            </li>;
+        })}
     </ul>;
 }
 
