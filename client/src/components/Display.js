e2b1cc88b38817827fab79bc25ed814620227cdc
AggieWB <usmaggie23@gmail.com>
2020-12-11 07:57:31 -0800

    Add img for each li item

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import xIcon from '../media/x-icon.png';
 
 //Add ability to remove single items and clear list
 const Display = (props) => {
     return <ul>
-        {props.list.map(item => <li key={item}>{item}</li>)}
+        {props.list.map(item => <li key={item}>{item}<img src={xIcon} alt={"A red X that can be clicked to remove this item from the list"}></img></li>)}
     </ul>;
 }
 
