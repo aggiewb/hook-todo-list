98122f5a1e45ace3d8b534f05b66bd77f890256c
AggieWB <usmaggie23@gmail.com>
2020-12-16 13:25:53 -0800

    Add test for rendering an Input component with an input value
    set to a valid string
    Import Input and Display components

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,7 @@
 import { shallow, mount } from 'enzyme';
 import App from './App';
+import Input from './components/Input';
+import Display from './components/Display';
 
 //https://jestjs.io/docs/en/expect
 //shallow: https://enzymejs.github.io/enzyme/docs/api/shallow.html
@@ -23,4 +25,12 @@ it('renders App class child components, and initializes their props', () => {
   expect(display.prop('list')).toEqual([]);
   expect(display.prop('removeItem')).toBeDefined();
   expect(display.prop('clearList')).toBeDefined();
+});
+
+it('renders an Input component with an input value set to a valid string', () => {
+  const EXPECTED_USER_INPUT = 'lorem ipsum';
+  const input = shallow(<Input text={EXPECTED_USER_INPUT} />);
+  const inputElement = input.find(`input[type='text']`);
+
+  expect(inputElement.prop('value')).toEqual(EXPECTED_USER_INPUT);
 });
